4d0d3e42081cab6b596bfce7d3625ee1762b4fc3
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-06-27 11:45:24 +0600

    15

---
+++ pushandreturn.js
@@ -0,0 +1,17 @@
+// question 1 push funcations how can do it i'm all ready forget it 
+
+// questin 2 start
+
+
+function person (name,email){
+    return{
+        name:name,
+        email:email
+
+    }
+}
+
+
+var Pi=person("Md Abujor Gifari", "mdabujorgifari046@gmial.com")
+
+console.log(Pi)
