6c5b30033a5c50bd8f61deea215895218395022f
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-06-27 12:09:27 +0600

    18

---
+++ 18 infinityloop.js
@@ -0,0 +1,10 @@
+for(; ; ){
+    var rand=Math.floor(Math.random()*100)
+    if(rand===17){
+        console.log("17 in found")
+        break
+    }
+    else{console.log("you have got a" + rand)
+
+    }
+}
