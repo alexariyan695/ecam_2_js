9ca0e51c973e641f03e776cf614426f23c99f750
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-06-27 12:26:08 +0600

    16

---
+++ 16 switch statement.js
@@ -0,0 +1,21 @@
+var today="A"
+switch(today){
+    case A:
+        console.log("Hey i'm a studnet")
+        break;
+        case B:
+            console.log("B is the second letter of the alphabet")
+            break;
+            case C:
+        console.log("Hey i'm a studnet")
+        break;
+        case D:
+        console.log("Hey i'm a studnet")
+        break;
+        case E:
+        console.log("Hey i'm a studnet")
+        break;
+}
+
+console.log("A")
+
