f5b4dbdb6d2dd3f7f62aecd8330131568b11a45e
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-06-27 12:40:01 +0600

    20

---
+++ 20 iterateobject.js
@@ -0,0 +1,28 @@
+var obj={
+    x:10,
+    y:10
+}
+if(obj.x===obj.y){
+    console.log("ture")
+
+}
+    else{
+        console.log("flase")
+    }
+
+// im using 2 way to cheack the valof of x and y the are both are eqaul or not equal to my Knowladge
+
+
+
+function obj2(x,y){
+    x=10
+    y=20
+    if(x===y){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(obj2(10,20))
